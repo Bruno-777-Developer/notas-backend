f55729206462f6c5f12bbde448a3014cfb35c31e
Bruno Silveira <bruno.silveira@sonner.com.br>
2021-02-05 16:20:28 -0300

    Contribuinte Funcionando!

--- src/main/java/br/com/sonner/notas/resources/ProdutoResources.java
+++ src/main/java/br/com/sonner/notas/resources/ProdutoResources.java
@@ -1,4 +1,34 @@
 package br.com.sonner.notas.resources;
 
+import br.com.sonner.notas.models.Produto;
+import br.com.sonner.notas.repository.ProdutoRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/produto")
+
 public class ProdutoResources {
+
+    final
+
+    ProdutoRepository  produtoRepository;
+
+    public ProdutoResources(ProdutoRepository produtoRepository){ this.produtoRepository = produtoRepository;}
+    @GetMapping("/")
+    public List<> listaProdutos(){
+        return produtoRepository.findAll();
+    }
+    @GetMapping("/{id}")
+    public Produto listaProduto(@PathVariable(value = "id") long id){
+        Produto produto =  produtoRepository.findById(id);
+        return produto;
+    }
+
+
+
 }
